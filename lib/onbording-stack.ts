fbc6ef2983baf43e8826806dedbefa987cc75941
Anupam Gupta <anupam.gupta@vaisala.com>
2023-04-08 10:34:20 +0300

    Added DynamoDB tables

--- lib/onbording-stack.ts
+++ lib/onbording-stack.ts
@@ -3,11 +3,45 @@ import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { LambdaIntegration, RestApi, Period } from "aws-cdk-lib/aws-apigateway";
+import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { Table, AttributeType } from "aws-cdk-lib/aws-dynamodb";
 import * as path from "path";
 
 export class OnbordingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create DynamoDB Tables
+
+    // Teams Table
+    const teamsTable = new Table(this, "teamsTable", {
+      tableName: "teams",
+      partitionKey: { name: "teamId", type: AttributeType.STRING },
+      sortKey: { name: "userId", type: AttributeType.STRING },
+      billingMode: cdk.aws_dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    teamsTable.addGlobalSecondaryIndex({
+      indexName: "UserIdIndex",
+      partitionKey: { name: "userId", type: AttributeType.STRING },
+      projectionType: cdk.aws_dynamodb.ProjectionType.ALL
+    });
+
+    // Tasks Table
+    const tasksTtable = new Table(this, "tasksTable", {
+      tableName: "tasks",
+      partitionKey: { name: "taskId", type: AttributeType.STRING },
+      sortKey: { name: "SK", type: AttributeType.STRING },
+      billingMode: cdk.aws_dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+    tasksTtable.addGlobalSecondaryIndex({
+      indexName: "TeamIdIndex",
+      partitionKey: { name: "teamId", type: AttributeType.STRING },
+      sortKey: { name: "taskId", type: AttributeType.STRING },
+      projectionType: cdk.aws_dynamodb.ProjectionType.ALL,
+    });
+    
 
     // Create SQS Queue
     const queue = new sqs.Queue(this, 'OnbordingQueue', {
@@ -65,5 +99,17 @@ export class OnbordingStack extends cdk.Stack {
 
     //Add POST method
     const postMethod = restApi.root.addMethod("POST", lambdaIntegration, { apiKeyRequired: false });
+
+    // State Machine
+    const startState = new stepfunctions.Pass(this, 'Start-processing', {
+      result: { value: "$[*]..['body','messageId']" }
+    });
+
+    const stateMachine = new stepfunctions.StateMachine(this, 'OnboardingStateMachine', {
+      definition: startState,
+      stateMachineType: stepfunctions.StateMachineType.STANDARD,
+      stateMachineName: "OnboardingStateMachine"
+    });
+
   }
 }
